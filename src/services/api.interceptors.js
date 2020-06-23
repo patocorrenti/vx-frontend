7c53b55110ed4b64476ab90120c18261b1a3f866
panterpato <patocorrenti@gmail.com>
2020-06-22 21:07:08 -0300

    auth headers in the interceptor

--- src/services/api.interceptors.js
+++ src/services/api.interceptors.js
@@ -4,10 +4,22 @@ import store from '../store'
 
 export default function setApiInterceptors() {
 
+  // Before request
+  api.interceptors.request.use(config => {
+    // Add access token
+    if (!config.headers.Authorization) {
+      const user = JSON.parse(localStorage.getItem('user'));
+      if (user && user.access) {
+        console.log('entro')
+        config.headers = { Authorization: `Bearer ${user.access}` };
+      }
+    }
+    return config
+  })
+
+  // After request
   api.interceptors.response.use(function (response) {
-  
     return response
-  
   }, function (error) {
 
     // Logout 
