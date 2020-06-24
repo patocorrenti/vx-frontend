65a49e5f346f285e2458e4547bf7858ae7a23beb
panterpato <patocorrenti@gmail.com>
2020-06-23 21:09:53 -0300

    installed vue cli s3 deploy plugin

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,35 @@
 module.exports = {
-    css: {
-      loaderOptions: {
-        sass: {
-          prependData: `
-            @import "@/styles/_fonts.scss";
-            @import "@/styles/_variables.scss";
-            @import "@/styles/_mixins.scss";
-            @import "@/styles/_base.scss";
-          `
-        }
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `
+          @import "@/styles/_fonts.scss";
+          @import "@/styles/_variables.scss";
+          @import "@/styles/_mixins.scss";
+          @import "@/styles/_base.scss";
+        `
       }
     }
+  },
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'us-east-1',
+      bucket: 'script-reader-media',
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'error.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
+  }
 };
